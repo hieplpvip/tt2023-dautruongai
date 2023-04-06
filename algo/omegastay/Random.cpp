cab60455097879349a8ca61bdd02ebbd053cab00
Le Bao Hiep <baohiep2013@gmail.com>
2023-04-06 11:30:51 +0700

    Use xoshiro128+ instead of xoshiro256+

--- algo/omegastay/Random.cpp
+++ algo/omegastay/Random.cpp
@@ -1,12 +1,12 @@
-// https://prng.di.unimi.it/xoshiro256plus.c
+// https://prng.di.unimi.it/xoshiro128plus.c
 
 #include "Random.h"
 
-static inline uint64_t rotl(const uint64_t x, int k) {
-  return (x << k) | (x >> (64 - k));
+static inline uint32_t rotl(const uint32_t x, int k) {
+  return (x << k) | (x >> (32 - k));
 }
 
-static uint64_t s[4];
+static uint32_t s[4];
 
 namespace Random {
   void seed(uint64_t x) {
@@ -19,15 +19,15 @@ namespace Random {
     }
   }
 
-  uint64_t next() {
-    const uint64_t result = s[0] + s[3];
-    const uint64_t t = s[1] << 17;
+  uint32_t next() {
+    const uint32_t result = s[0] + s[3];
+    const uint32_t t = s[1] << 9;
     s[2] ^= s[0];
     s[3] ^= s[1];
     s[1] ^= s[2];
     s[0] ^= s[3];
     s[2] ^= t;
-    s[3] = rotl(s[3], 45);
+    s[3] = rotl(s[3], 11);
     return result;
   }
 
--- algo/omegastay/Random.h
+++ algo/omegastay/Random.h
@@ -7,13 +7,13 @@ namespace Random {
   // Seed the random number generator
   void seed(uint64_t x);
 
-  // Return a random 64-bit integer using xoshiro256+
-  uint64_t next();
+  // Return a random 32-bit integer using xoshiro128+
+  uint32_t next();
 
-  // Return a random integer in [l, r] using xoshiro256+
+  // Return a random integer in [l, r] using xoshiro128+
   uint32_t rand(uint32_t l, uint32_t r);
 
-  // Return a random integer in [0, n - 1] using xoshiro256+
+  // Return a random integer in [0, n - 1] using xoshiro128+
   uint32_t rand(uint32_t n);
 };
 
