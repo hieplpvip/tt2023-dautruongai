f6e54607515d34b95f0977c2d478e76a3485f859
Le Bao Hiep <baohiep2013@gmail.com>
2023-04-06 10:24:44 +0700

    Parallelize run_many

--- judge/run_many.py
+++ judge/run_many.py
@@ -2,6 +2,7 @@
 
 import argparse
 import os
+from multiprocessing import Pool
 
 from beautifultable import BeautifulTable
 
@@ -9,16 +10,20 @@ from match import Match, MatchStatus
 from utility import *
 
 
-def main():
-    parser = argparse.ArgumentParser(description='Run a match between two agents')
-    parser.add_argument('map_list', type=str, help='Path to map list file')
-    parser.add_argument('exe0', type=str, help='Path to first agent')
-    parser.add_argument('exe1', type=str, help='Path to second agent')
-    parser.add_argument('--times', type=int, default=1, help='Number of times to run for each map')
-    parser.add_argument('--no-color', default=False, action='store_true', help='Disable color output')
-    parser.add_argument('--no-log', default=False, action='store_true', help='Disable logging')
-    args = parser.parse_args()
+def print_result(exe0, exe1, maps, result):
+    table = BeautifulTable(maxwidth=9999)
+    for map_filename in maps:
+        table.rows.append([
+            result[map_filename]['first_win'],
+            result[map_filename]['second_win'],
+            result[map_filename]['draw'],
+        ])
+    table.rows.header = maps
+    table.columns.header = [f'{exe0} wins', f'{exe1} wins', 'draw']
+    print(table)
 
+
+def single_thread(args):
     if args.no_color:
         disable_color()
 
@@ -34,7 +39,7 @@ def main():
         maps = []
         for line in f.readlines():
             line = line.strip()
-            if not line.startswith('#'):
+            if line and not line.startswith('#'):
                 maps.append(line)
 
     result = {}
@@ -65,16 +70,75 @@ def main():
             elif match.status == MatchStatus.DRAW:
                 result[map_filename]['draw'] += 1
 
-    table = BeautifulTable(maxwidth=9999)
+    print_result(args.exe0, args.exe1, maps, result)
+
+
+def run_job(map_filename, exe0, exe1):
+    match = Match(map_filename, exe0, exe1)
+    while not match.finished:
+        match.next_turn()
+
+    if match.status == MatchStatus.FIRST_WIN:
+        return 'first_win'
+    elif match.status == MatchStatus.SECOND_WIN:
+        return 'second_win'
+    elif match.status == MatchStatus.DRAW:
+        return 'draw'
+
+
+def multi_threads(args):
+    disable_color()
+    disable_all_logging()
+
+    exe0 = os.path.abspath(args.exe0)
+    exe1 = os.path.abspath(args.exe1)
+    times = args.times
+    with open(args.map_list, 'r', encoding='utf8') as f:
+        maps = []
+        for line in f.readlines():
+            line = line.strip()
+            if line and not line.startswith('#'):
+                maps.append(line)
+
+    maps_abs_path = [
+        os.path.abspath(os.path.join(os.path.dirname(args.map_list), map_filename)) for map_filename in maps
+    ]
+
+    jobs = list(zip(maps_abs_path * times, [exe0] * len(maps_abs_path) * times, [exe1] * len(maps_abs_path) * times))
+
+    with Pool(args.num_threads) as p:
+        job_results = p.starmap(run_job, jobs)
+
+    result = {}
     for map_filename in maps:
-        table.rows.append([
-            result[map_filename]['first_win'],
-            result[map_filename]['second_win'],
-            result[map_filename]['draw'],
-        ])
-    table.rows.header = maps
-    table.columns.header = [f'{args.exe0} wins', f'{args.exe1} wins', 'draw']
-    print(table)
+        result[map_filename] = {
+            'first_win': 0,
+            'second_win': 0,
+            'draw': 0,
+        }
+    for map_filename, job_result in zip(maps * times, job_results):
+        result[map_filename][job_result] += 1
+
+    print_result(args.exe0, args.exe1, maps, result)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Run a match between two agents')
+    parser.add_argument('map_list', type=str, help='Path to map list file')
+    parser.add_argument('exe0', type=str, help='Path to first agent')
+    parser.add_argument('exe1', type=str, help='Path to second agent')
+    parser.add_argument('--times', type=int, default=1, help='Number of times to run for each map')
+    parser.add_argument('--num-threads', type=int, default=1, help='Number of threads')
+    parser.add_argument('--no-color', default=False, action='store_true', help='Disable color output')
+    parser.add_argument('--no-log', default=False, action='store_true', help='Disable logging')
+    args = parser.parse_args()
+
+    assert args.num_threads > 0, 'Number of threads must be greater than 0'
+
+    if args.num_threads == 1:
+        single_thread(args)
+    else:
+        multi_threads(args)
 
 
 if __name__ == '__main__':
