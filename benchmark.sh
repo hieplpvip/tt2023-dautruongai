f9b9bb31de46a378cf58869fbe3176cb67a4dc61
Le Bao Hiep <baohiep2013@gmail.com>
2023-04-08 09:25:10 +0700

    Update benchmark scripts

--- benchmark.sh
+++ benchmark.sh
@@ -5,10 +5,16 @@ echo "YOU HAVE BEEN WARNED"
 
 rm -rf results && mkdir results
 
+judge/run_many.py maps/list_semifinal.txt bin/omegastay    bin/oh-my-blue               --times 20 --num-threads 10 >results/omegastay_vs_ohmyblue.txt
 judge/run_many.py maps/list_semifinal.txt bin/omegastay    bin/granitestate             --times 20 --num-threads 10 >results/omegastay_vs_granitestate.txt
 judge/run_many.py maps/list_semifinal.txt bin/omegastay    bin/babyblue                 --times 20 --num-threads 10 >results/omegastay_vs_babyblue.txt
 judge/run_many.py maps/list_semifinal.txt bin/omegastay    bin/heisenberg               --times 20 --num-threads 10 >results/omegastay_vs_heisenberg.txt
 judge/run_many.py maps/list_semifinal.txt bin/omegastay    bin/tahp-estimate-shield-val --times 20 --num-threads 10 >results/omegastay_vs_tahp.txt
+
+judge/run_many.py maps/list_semifinal.txt bin/oh-my-blue   bin/granitestate             --times 20 --num-threads 10 >results/ohmyblue_vs_granitestate.txt
+judge/run_many.py maps/list_semifinal.txt bin/oh-my-blue   bin/babyblue                 --times 20 --num-threads 10 >results/ohmyblue_vs_babyblue.txt
+judge/run_many.py maps/list_semifinal.txt bin/oh-my-blue   bin/heisenberg               --times 20 --num-threads 10 >results/ohmyblue_vs_heisenberg.txt
+judge/run_many.py maps/list_semifinal.txt bin/oh-my-blue   bin/tahp-estimate-shield-val --times 20 --num-threads 10 >results/ohmyblue_vs_tahp.txt
 
 judge/run_many.py maps/list_semifinal.txt bin/granitestate bin/babyblue                 --times 20 --num-threads 10 >results/granitestate_vs_babyblue.txt
 judge/run_many.py maps/list_semifinal.txt bin/granitestate bin/heisenberg               --times 20 --num-threads 10 >results/granitestate_vs_heisenberg.txt
