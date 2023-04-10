4d2a2e981da75fafa261a8dbfb86f9760d603341
Le Bao Hiep <baohiep2013@gmail.com>
2023-04-11 04:38:30 +0700

    omegastay: only keep candidates adjacent to gold/shield

--- algo/omegastay/Heuristic.h
+++ algo/omegastay/Heuristic.h
@@ -10,16 +10,6 @@ namespace Heuristic {
    * Get candidates for optimal first place.
    */
   std::vector<std::pair<score_t, Position>> GetCandidates(State& state, int numCandidates);
-
-  /*
-   * Get the highest heat value corresponding to player.
-   */
-  score_t GetHighestHeat(State& state, PlayerEnum player);
-
-  /*
-   * Evaluate the score of a cell.
-   */
-  score_t Evaluate(double gold, int distance);
 }
 
 #endif
