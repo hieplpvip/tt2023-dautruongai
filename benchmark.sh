7bba14e729372266a9db34529f2a512313a3fd4c
Le Bao Hiep <baohiep2013@gmail.com>
2023-04-12 07:33:18 +0700

    update benchmark script

--- benchmark.sh
+++ benchmark.sh
@@ -5,6 +5,12 @@ echo "YOU HAVE BEEN WARNED"
 
 rm -rf results && mkdir results
 
+judge/run_many.py maps/list_semifinal.txt bin/felina       bin/oh-my-blue               --times 20 --num-threads 10 >results/felina_vs_ohmyblue.txt
+judge/run_many.py maps/list_semifinal.txt bin/felina       bin/granitestate             --times 20 --num-threads 10 >results/felina_vs_granitestate.txt
+judge/run_many.py maps/list_semifinal.txt bin/felina       bin/babyblue                 --times 20 --num-threads 10 >results/felina_vs_babyblue.txt
+judge/run_many.py maps/list_semifinal.txt bin/felina       bin/heisenberg               --times 20 --num-threads 10 >results/felina_vs_heisenberg.txt
+judge/run_many.py maps/list_semifinal.txt bin/felina       bin/tahp-estimate-shield-val --times 20 --num-threads 10 >results/felina_vs_tahp.txt
+
 judge/run_many.py maps/list_semifinal.txt bin/omegastay    bin/oh-my-blue               --times 20 --num-threads 10 >results/omegastay_vs_ohmyblue.txt
 judge/run_many.py maps/list_semifinal.txt bin/omegastay    bin/granitestate             --times 20 --num-threads 10 >results/omegastay_vs_granitestate.txt
 judge/run_many.py maps/list_semifinal.txt bin/omegastay    bin/babyblue                 --times 20 --num-threads 10 >results/omegastay_vs_babyblue.txt
