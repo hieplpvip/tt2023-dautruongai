e69fe4480819d38912c431d9a5525278bfe82dc4
Le Bao Hiep <baohiep2013@gmail.com>
2023-04-12 18:24:52 +0700

    fix

--- benchmark.sh
+++ benchmark.sh
@@ -3,8 +3,30 @@
 echo "DO NOT QUIT THIS SCRIPT UNTIL IT IS FINISHED"
 echo "YOU HAVE BEEN WARNED"
 
+rm -rf results && mkdir results
+
+judge/run_many.py maps/list_semifinal.txt bin/felina       bin/oh-my-blue               --times 20 --num-threads 10 >results/felina_vs_ohmyblue.txt
+judge/run_many.py maps/list_semifinal.txt bin/felina       bin/granitestate             --times 20 --num-threads 10 >results/felina_vs_granitestate.txt
+judge/run_many.py maps/list_semifinal.txt bin/felina       bin/babyblue                 --times 20 --num-threads 10 >results/felina_vs_babyblue.txt
+judge/run_many.py maps/list_semifinal.txt bin/felina       bin/heisenberg               --times 20 --num-threads 10 >results/felina_vs_heisenberg.txt
+judge/run_many.py maps/list_semifinal.txt bin/felina       bin/tahp-estimate-shield-val --times 20 --num-threads 10 >results/felina_vs_tahp.txt
+
 judge/run_many.py maps/list_semifinal.txt bin/omegastay    bin/oh-my-blue               --times 20 --num-threads 10 >results/omegastay_vs_ohmyblue.txt
 judge/run_many.py maps/list_semifinal.txt bin/omegastay    bin/granitestate             --times 20 --num-threads 10 >results/omegastay_vs_granitestate.txt
 judge/run_many.py maps/list_semifinal.txt bin/omegastay    bin/babyblue                 --times 20 --num-threads 10 >results/omegastay_vs_babyblue.txt
 judge/run_many.py maps/list_semifinal.txt bin/omegastay    bin/heisenberg               --times 20 --num-threads 10 >results/omegastay_vs_heisenberg.txt
 judge/run_many.py maps/list_semifinal.txt bin/omegastay    bin/tahp-estimate-shield-val --times 20 --num-threads 10 >results/omegastay_vs_tahp.txt
+
+judge/run_many.py maps/list_semifinal.txt bin/oh-my-blue   bin/granitestate             --times 20 --num-threads 10 >results/ohmyblue_vs_granitestate.txt
+judge/run_many.py maps/list_semifinal.txt bin/oh-my-blue   bin/babyblue                 --times 20 --num-threads 10 >results/ohmyblue_vs_babyblue.txt
+judge/run_many.py maps/list_semifinal.txt bin/oh-my-blue   bin/heisenberg               --times 20 --num-threads 10 >results/ohmyblue_vs_heisenberg.txt
+judge/run_many.py maps/list_semifinal.txt bin/oh-my-blue   bin/tahp-estimate-shield-val --times 20 --num-threads 10 >results/ohmyblue_vs_tahp.txt
+
+judge/run_many.py maps/list_semifinal.txt bin/granitestate bin/babyblue                 --times 20 --num-threads 10 >results/granitestate_vs_babyblue.txt
+judge/run_many.py maps/list_semifinal.txt bin/granitestate bin/heisenberg               --times 20 --num-threads 10 >results/granitestate_vs_heisenberg.txt
+judge/run_many.py maps/list_semifinal.txt bin/granitestate bin/tahp-estimate-shield-val --times 20 --num-threads 10 >results/granitestate_vs_tahp.txt
+
+judge/run_many.py maps/list_semifinal.txt bin/babyblue     bin/heisenberg               --times 20 --num-threads 10 >results/babyblue_vs_heisenberg.txt
+judge/run_many.py maps/list_semifinal.txt bin/babyblue     bin/tahp-estimate-shield-val --times 20 --num-threads 10 >results/babyblue_vs_tahp.txt
+
+judge/run_many.py maps/list_semifinal.txt bin/heisenberg   bin/tahp-estimate-shield-val --times 20 --num-threads 10 >results/heisenberg_vs_tahp.txt
