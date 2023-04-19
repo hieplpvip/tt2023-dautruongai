130398d5a563041e599f628305790c1caf0fdcb7
Le Bao Hiep <baohiep2013@gmail.com>
2023-04-19 07:30:04 +0700

    New benchmark

--- benchmark.sh
+++ benchmark.sh
@@ -12,6 +12,7 @@ judge/run_many.py maps/list_official.txt bin/felina       bin/babyblue          
 judge/run_many.py maps/list_official.txt bin/felina       bin/heisenberg               --times 20 --num-threads 10 >results/felina_vs_heisenberg.txt
 judge/run_many.py maps/list_official.txt bin/felina       bin/tahp-estimate-shield-val --times 20 --num-threads 10 >results/felina_vs_tahp.txt
 judge/run_many.py maps/list_official.txt bin/felina       bin/zhong                    --times 20 --num-threads 10 >results/felina_vs_zhong.txt
+judge/run_many.py maps/list_official.txt bin/felina       bin/renamed                  --times 20 --num-threads 10 >results/felina_vs_renamed.txt
 
 judge/run_many.py maps/list_official.txt bin/omegastay    bin/oh-my-blue               --times 20 --num-threads 10 >results/omegastay_vs_ohmyblue.txt
 judge/run_many.py maps/list_official.txt bin/omegastay    bin/granitestate             --times 20 --num-threads 10 >results/omegastay_vs_granitestate.txt
@@ -19,21 +20,26 @@ judge/run_many.py maps/list_official.txt bin/omegastay    bin/babyblue          
 judge/run_many.py maps/list_official.txt bin/omegastay    bin/heisenberg               --times 20 --num-threads 10 >results/omegastay_vs_heisenberg.txt
 judge/run_many.py maps/list_official.txt bin/omegastay    bin/tahp-estimate-shield-val --times 20 --num-threads 10 >results/omegastay_vs_tahp.txt
 judge/run_many.py maps/list_official.txt bin/omegastay    bin/zhong                    --times 20 --num-threads 10 >results/omegastay_vs_zhong.txt
+judge/run_many.py maps/list_official.txt bin/omegastay    bin/renamed                  --times 20 --num-threads 10 >results/omegastay_vs_renamed.txt
 
 judge/run_many.py maps/list_official.txt bin/oh-my-blue   bin/granitestate             --times 20 --num-threads 10 >results/ohmyblue_vs_granitestate.txt
 judge/run_many.py maps/list_official.txt bin/oh-my-blue   bin/babyblue                 --times 20 --num-threads 10 >results/ohmyblue_vs_babyblue.txt
 judge/run_many.py maps/list_official.txt bin/oh-my-blue   bin/heisenberg               --times 20 --num-threads 10 >results/ohmyblue_vs_heisenberg.txt
 judge/run_many.py maps/list_official.txt bin/oh-my-blue   bin/tahp-estimate-shield-val --times 20 --num-threads 10 >results/ohmyblue_vs_tahp.txt
 judge/run_many.py maps/list_official.txt bin/oh-my-blue   bin/zhong                    --times 20 --num-threads 10 >results/ohmyblue_vs_zhong.txt
+judge/run_many.py maps/list_official.txt bin/oh-my-blue   bin/renamed                  --times 20 --num-threads 10 >results/ohmyblue_vs_renamed.txt
 
 judge/run_many.py maps/list_official.txt bin/granitestate bin/babyblue                 --times 20 --num-threads 10 >results/granitestate_vs_babyblue.txt
 judge/run_many.py maps/list_official.txt bin/granitestate bin/heisenberg               --times 20 --num-threads 10 >results/granitestate_vs_heisenberg.txt
 judge/run_many.py maps/list_official.txt bin/granitestate bin/tahp-estimate-shield-val --times 20 --num-threads 10 >results/granitestate_vs_tahp.txt
 judge/run_many.py maps/list_official.txt bin/granitestate bin/zhong                    --times 20 --num-threads 10 >results/granitestate_vs_zhong.txt
+judge/run_many.py maps/list_official.txt bin/granitestate bin/renamed                  --times 20 --num-threads 10 >results/granitestate_vs_renamed.txt
 
 judge/run_many.py maps/list_official.txt bin/babyblue     bin/heisenberg               --times 20 --num-threads 10 >results/babyblue_vs_heisenberg.txt
 judge/run_many.py maps/list_official.txt bin/babyblue     bin/tahp-estimate-shield-val --times 20 --num-threads 10 >results/babyblue_vs_tahp.txt
 judge/run_many.py maps/list_official.txt bin/babyblue     bin/zhong                    --times 20 --num-threads 10 >results/babyblue_vs_zhong.txt
+judge/run_many.py maps/list_official.txt bin/babyblue     bin/renamed                  --times 20 --num-threads 10 >results/babyblue_vs_renamed.txt
 
 judge/run_many.py maps/list_official.txt bin/heisenberg   bin/tahp-estimate-shield-val --times 20 --num-threads 10 >results/heisenberg_vs_tahp.txt
 judge/run_many.py maps/list_official.txt bin/heisenberg   bin/zhong                    --times 20 --num-threads 10 >results/heisenberg_vs_zhong.txt
+judge/run_many.py maps/list_official.txt bin/heisenberg   bin/renamed                  --times 20 --num-threads 10 >results/heisenberg_vs_renamed.txt
