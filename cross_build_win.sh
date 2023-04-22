32c06f353ff29815bd729b3da30aa7c901c0d61f
Le Bao Hiep <baohiep2013@gmail.com>
2023-04-22 09:32:55 +0700

    Add option to tune for x86

--- cross_build_win.sh
+++ cross_build_win.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cmake -S . -B build -D CMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=mingw-w64-toolchain.cmake -DPRINT_FROM_SENO_WITH_LOVE=ON
+cmake -S . -B build -D CMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=mingw-w64-toolchain.cmake -DPRINT_FROM_SENO_WITH_LOVE=ON -DTUNE_FOR_X86=ON
 cmake --build build --verbose -j$(nproc)
